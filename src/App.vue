cda03a2b04b85cb8ea6de0b3908d2b95174394ca
filippo199600 <pippotartaglia@gmail.com>
2023-09-19 12:27:32 +0200

    add store and rules reset style

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <script>
+import { store } from "./data/store";
+
 import HeaderApp from "./components/HeaderApp.vue";
 import MainApp from "./components/MainApp.vue";
 import FooterApp from "./components/FooterApp.vue";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
   components: { HeaderApp, MainApp, FooterApp },
 };
 </script>
 
 <template>
   <div class="container">
-    <h1>inizio progetto</h1>
+    <h1>{{ store.titleReactive }}</h1>
   </div>
   <HeaderApp />
   <MainApp />
