e03bbda6828363c54b33a36745c548c5706a5c14
filippo199600 <pippotartaglia@gmail.com>
2023-09-21 17:37:39 +0200

    edit style

--- src/components/sectionsMain/JumboSection.vue
+++ src/components/sectionsMain/JumboSection.vue
@@ -83,14 +83,14 @@ export default {
           data-bs-ride="carousel"
         >
           <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="1500">
+            <div class="carousel-item active" data-bs-interval="3000">
               <img
                 v-for="item in imageCarusel"
                 :src="'../../../public/img/logos/' + item"
                 alt=""
               />
             </div>
-            <div class="carousel-item" data-bs-interval="1500">
+            <div class="carousel-item" data-bs-interval="3000">
               <img
                 v-for="item in imageCarusel"
                 :src="'../../../public/img/logos/' + item"
