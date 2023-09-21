e881c6e25b44a5fc8c53d76158878accf749b12e
filippo199600 <pippotartaglia@gmail.com>
2023-09-21 09:44:55 +0200

    esit style

--- src/components/sectionsFooter/Credits.vue
+++ src/components/sectionsFooter/Credits.vue
@@ -35,55 +35,58 @@ export default {
 <template>
   <div class="wrapper">
     <div class="container">
-      <div class="left-content">
-        <div class="adress">
-          <h5>{{ footerList.adress.title }}</h5>
-          <p>{{ footerList.adress.location }}</p>
-          <p>{{ footerList.adress.phone }}</p>
-          <p>{{ footerList.adress.email }}</p>
+      <div class="footer-content">
+        <div class="left-content">
+          <div class="adress">
+            <h5>{{ footerList.adress.title }}</h5>
+            <p>{{ footerList.adress.location }}</p>
+            <p>{{ footerList.adress.phone }}</p>
+            <p>{{ footerList.adress.email }}</p>
+          </div>
+          <div class="social">
+            <img src="../../../public/img/social/facebook.png" alt="" />
+            <img src="../../../public/img/social/twitter.svg" alt="" />
+            <img src="../../../public/img/social/instagram.svg" alt="" />
+            <img src="../../../public/img/social/linkedin.svg" alt="" />
+          </div>
         </div>
-        <div class="social">
-          <img src="../../../public/img/social/facebook.png" alt="" />
-          <img src="../../../public/img/social/twitter.svg" alt="" />
-          <img src="../../../public/img/social/instagram.svg" alt="" />
-          <img src="../../../public/img/social/linkedin.svg" alt="" />
+        <div class="right-content">
+          <div class="explore">
+            <h5>{{ footerList.explore.title }}</h5>
+            <ul>
+              <li
+                v-for="(item, index) in footerList.explore.items"
+                :key="footerList.explore.items[index]"
+              >
+                {{ item }}
+              </li>
+            </ul>
+          </div>
+          <div class="tutoring">
+            <h5>{{ footerList.tutoring.title }}</h5>
+            <ul>
+              <li
+                v-for="(detail, i) in footerList.tutoring.items"
+                :key="footerList.tutoring.items[i]"
+              >
+                {{ detail }}
+              </li>
+            </ul>
+          </div>
+          <div class="information">
+            <h5>{{ footerList.information.title }}</h5>
+            <ul>
+              <li
+                v-for="(content, a) in footerList.information.items"
+                :key="footerList.information.items[a]"
+              >
+                {{ content }}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div class="right-content">
-        <div class="explore">
-          <h5>{{ footerList.explore.title }}</h5>
-          <ul>
-            <li
-              v-for="(item, index) in footerList.explore.items"
-              :key="footerList.explore.items[index]"
-            >
-              {{ item }}
-            </li>
-          </ul>
-        </div>
-        <div class="tutoring">
-          <h5>{{ footerList.tutoring.title }}</h5>
-          <ul>
-            <li
-              v-for="(detail, i) in footerList.tutoring.items"
-              :key="footerList.tutoring.items[i]"
-            >
-              {{ detail }}
-            </li>
-          </ul>
-        </div>
-        <div class="information">
-          <h5>{{ footerList.information.title }}</h5>
-          <ul>
-            <li
-              v-for="(content, a) in footerList.information.items"
-              :key="footerList.information.items[a]"
-            >
-              {{ content }}
-            </li>
-          </ul>
-        </div>
-      </div>
+      <p class="text-center">© 2020 Maxcoach. All Rights Reverved</p>
     </div>
   </div>
 </template>
@@ -91,53 +94,59 @@ export default {
 <style lang="scss" scoped>
 .wrapper {
   background-color: #f5f7fa;
-  padding: 100px 0;
+  padding: 100px 0 50px;
   .container {
-    display: flex;
-    justify-content: space-around;
-    .left-content {
-      .adress {
-        h5 {
-          color: #353535;
+    .footer-content {
+      display: flex;
+      justify-content: space-around;
+      .left-content {
+        .adress {
+          h5 {
+            color: #353535;
+          }
+          p {
+            color: #6e6e6e;
+          }
         }
-        p {
+        .social {
           color: #6e6e6e;
+          display: flex;
+          gap: 20px;
+
+          img {
+            height: 20px;
+            width: 20px;
+          }
         }
       }
-      .social {
-        color: #6e6e6e;
+      .right-content {
         display: flex;
-        gap: 20px;
+        gap: 60px;
 
-        img {
-          height: 20px;
-          width: 20px;
+        .tutoring {
+          padding-top: 26px;
+        }
+
+        h5 {
+          color: #353535;
+          margin-bottom: 30px;
+        }
+
+        ul {
+          padding: 0;
+
+          li {
+            color: #6e6e6e;
+            list-style: none;
+            padding: 0;
+            margin-bottom: 20px;
+          }
         }
       }
     }
-    .right-content {
-      display: flex;
-      gap: 60px;
-
-      .tutoring {
-        padding-top: 26px;
-      }
-
-      h5 {
-        color: #353535;
-        margin-bottom: 30px;
-      }
-
-      ul {
-        padding: 0;
-
-        li {
-          color: #6e6e6e;
-          list-style: none;
-          padding: 0;
-          margin-bottom: 20px;
-        }
-      }
+    .text-center {
+      color: #6e6e6e;
+      margin-top: 70px;
     }
   }
 }
